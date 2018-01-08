4c0a3739d809efcb6ae79fc0184c72ed23d81037
wenji-my <my841686869@163.com>
2018-01-08 15:09:00 +0800

    Restructure Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';//重构后不需要Component
 
-export default class Header extends Component {
-    render() {
+// export default class Header extends Component {
+//     render() {
+//
+//         return (
+//             <div className={"container"}>
+//                 <div className={"row"}>
+//                     <div className={"col-xs-11 col-xs-offset-11"}>
+//                         <h1>Header</h1>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
+//重构代码（无状态的组件）
+const Header = (props) => {
         return (
             <div className={"container"}>
                 <div className={"row"}>
@@ -12,6 +27,6 @@ export default class Header extends Component {
                 </div>
             </div>
         );
-    }
-}
+};
 
+export default Header;
